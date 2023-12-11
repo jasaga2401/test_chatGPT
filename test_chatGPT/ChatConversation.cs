809097a57b5a2ca987151a61eeacc8f4e1206599
jasaga2401 <jasaga@protonmail.com>
2023-12-11 22:27:57 +0000

    Add project files.

---
+++ test_chatGPT/ChatConversation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace test_chatGPT
+{
+    internal class ChatConversation
+    {
+        internal void AppendUserMessage(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Task GetChatCompletionAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test_chatGPT/Form1.cs
@@ -0,0 +1,32 @@
+﻿using OpenAI_API;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test_chatGPT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_LoadAsync(object sender, EventArgs e)
+        {
+
+            var openai = new OpenAIAPI(new APIAuthentication("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"));
+            var conversation = openai.Chat.CreateConversation();
+            conversation.AppendUserInput("How to write to console: ");
+            var response = await conversation.GetResponseFromChatbotAsync();
+            label1.Text = response.ToString();
+
+        }
+    }
+}
